bab867213c30dd7212b2215ae51d7d51fb5ad51f
Pallek Kankanamalage Don Gayathra Shan Siriwardana <shansiriwardana@Palleks-MacBook-Air.local>
2023-04-05 22:55:19 +0800

    test

--- main.tf
+++ main.tf
@@ -7,21 +7,21 @@ module "networking" {
   nat                 = var.nat
 }
 
-# module "database" {
-#   source               = "./modules/database"
-#   db_instance_name     = var.db_instance_name
-#   db_version           = var.db_version
-#   db_instance_type     = var.db_instance_type
-#   db_availability_type = var.db_availability_type
-#   db_disk_size         = var.db_disk_size
-#   app_database         = var.app_database
-#   private_network      = module.networking.network_id
-#   db_username          = var.db_username
-#   db_password          = var.db_password
-#   depends_on = [
-#     module.networking
-#   ]
-# }
+module "database" {
+  source               = "./modules/database"
+  db_instance_name     = var.db_instance_name
+  db_version           = var.db_version
+  db_instance_type     = var.db_instance_type
+  db_availability_type = var.db_availability_type
+  db_disk_size         = var.db_disk_size
+  app_database         = var.app_database
+  private_network      = module.networking.network_id
+  db_username          = var.db_username
+  db_password          = var.db_password
+  depends_on = [
+    module.networking
+  ]
+}
 
 # module "kubernetes" {
 #   source                  = "./modules/kubernetes"
