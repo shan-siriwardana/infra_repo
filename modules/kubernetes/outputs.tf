2062dde2d2c639b07b95566031ba9694d674df3e
Pallek Kankanamalage Don Gayathra Shan Siriwardana <shansiriwardana@Palleks-MacBook-Air.local>
2023-04-06 01:20:54 +0800

    test

--- modules/kubernetes/outputs.tf
+++ modules/kubernetes/outputs.tf
@@ -2,3 +2,8 @@ output "gke_cluster_name" {
   value       = google_container_cluster.primary.name
   description = "The GKE cluster name"
 }
+
+output "artifactory_name" {
+  value       = google_artifact_registry_repository.demoapp_repo.name
+  description = "The GKE cluster name"
+}
