2c829756ff89a75f4f1b5f706469472d3519685f
Pallek Kankanamalage Don Gayathra Shan Siriwardana <shansiriwardana@Palleks-MacBook-Air.local>
2023-04-05 23:44:59 +0800

    test

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -85,5 +85,6 @@ resource "google_compute_network_peering_routes_config" "peering_routes" {
   peering = google_service_networking_connection.private_vpc_connection.peering
   network = google_compute_network.network.name
 
+  import_custom_routes = false
   export_custom_routes = true
 }
