9e0e804b48cc3b101daf5bfc3bd0f23b38b7f876
Pallek Kankanamalage Don Gayathra Shan Siriwardana <shansiriwardana@Palleks-MacBook-Air.local>
2023-04-05 22:23:58 +0800

    test

--- main.tf
+++ main.tf
@@ -7,33 +7,33 @@ module "networking" {
   nat                 = var.nat
 }
 
-module "database" {
-  source               = "./modules/database"
-  db_instance_name     = var.db_instance_name
-  db_version           = var.db_version
-  db_instance_type     = var.db_instance_type
-  db_availability_type = var.db_availability_type
-  db_disk_size         = var.db_disk_size
-  app_database         = var.app_database
-  private_network      = module.networking.network_id
-  db_username          = var.db_username
-  db_password          = var.db_password
-  depends_on = [
-    module.networking
-  ]
-}
+# module "database" {
+#   source               = "./modules/database"
+#   db_instance_name     = var.db_instance_name
+#   db_version           = var.db_version
+#   db_instance_type     = var.db_instance_type
+#   db_availability_type = var.db_availability_type
+#   db_disk_size         = var.db_disk_size
+#   app_database         = var.app_database
+#   private_network      = module.networking.network_id
+#   db_username          = var.db_username
+#   db_password          = var.db_password
+#   depends_on = [
+#     module.networking
+#   ]
+# }
 
-module "kubernetes" {
-  source                  = "./modules/kubernetes"
-  cluster_name            = var.cluster_name
-  cluster_master_location = var.cluster_master_location
-  cluster_network         = module.networking.network_id
-  node_pool_location          = var.node_pool_location
-  initial_node_count      = var.initial_node_count
-  project_id              = var.project_id
-  cluster_instance_subnet = var.cluster_instance_subnet
-  depends_on = [
-    module.networking
-  ]
-}
+# module "kubernetes" {
+#   source                  = "./modules/kubernetes"
+#   cluster_name            = var.cluster_name
+#   cluster_master_location = var.cluster_master_location
+#   cluster_network         = module.networking.network_id
+#   node_pool_location          = var.node_pool_location
+#   initial_node_count      = var.initial_node_count
+#   project_id              = var.project_id
+#   cluster_instance_subnet = var.cluster_instance_subnet
+#   depends_on = [
+#     module.networking
+#   ]
+# }
 
